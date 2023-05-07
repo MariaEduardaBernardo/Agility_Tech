a5c97baf8b467bbdefca971a41c7295e89196012
Maria Eduarda <mariaedu9434@gmail.com>
2023-05-06 21:52:19 -0300

    Updates pages

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,44 @@ function openModal() {
 function closeModal() {
     document.getElementById("myModal").style.display = "none";
 }
+
+/*Formulario de contato*/
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const subject = document.querySelector('#subject').value;
+    const message = document.querySelector('#message').value;
+
+    // Código para enviar e-mail
+});
+
+/* Carrossel com as ONGs na home*/
+$(document).ready(function(){
+    $('.Ex_ONGs').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+        ]
+    });
+});
